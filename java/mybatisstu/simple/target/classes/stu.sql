b8701ac2612a522951e4cfb2dda1b6c34ec05913
hym <375416934@qq.com>
2019-05-23 11:22:41 +0800

    delete target folder

--- java/mybatisstu/simple/target/classes/stu.sql
+++
@@ -1,8 +0,0 @@
-create database stu;
-
-create table dept(
-  deptno int auto_increment,
-  deptname varchar(100),
-  location varchar(200),
-  primary key (deptno)
-)DEFAULT CHARSET=utf8;
---
+++ java/mybatisstu/typehandler/src/main/resources/addColumn.sql
@@ -0,0 +1,1 @@
+alter table stu.dept add column flag int;
