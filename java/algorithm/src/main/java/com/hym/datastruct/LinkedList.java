207d1c700f4a86c8b2909cb9c4b4724eb89271e9
hym <375416934@qq.com>
2019-07-19 17:39:00 +0800

    add files

--- java/algorithm/src/main/java/com/hym/datastruct/LinkedList.java
+++ java/algorithm/src/main/java/com/hym/datastruct/LinkedList.java
@@ -14,7 +14,7 @@ public class LinkedList {
         Node next;
         Node(int d)  { data = d;  next=null; }
     }
-    
+
     public static void main(String[] args){
         LinkedList ll = new LinkedList();
         ll.head = new Node(1);
@@ -57,10 +57,70 @@ public class LinkedList {
         +----+------+     +----+------+     +----+------+ */
 
 
-        printList(ll.head);
+        ll.printList(ll.head);
+
+        System.out.println("==================");
+
+        //A node can be added in three ways
+        //1) At the front of the linked list
+        Node node0 = ll.push(0);
+        ll.printList(node0);
+
+        System.out.println("==================");
+
+        //Add a node after a given node
+        ll.insertAfter(node0.next.next,23);
+        ll.printList(node0);
+
+        System.out.println("==================");
+
+        //Add a node at the end
+        ll.append(node0, 4);
+        ll.printList(node0);
     }
 
-    public static void printList(Node n){
+    private Node push(int data) {
+        Node node0 = new Node(0);
+        node0.next = head;
+        return node0;
+    }
+
+    private void insertAfter(Node prevNode, int data) {
+        if (prevNode == null) { throw new RuntimeException("The given previous node cannot be null");}
+        Node newNode = new Node(data);
+        //Make next of new Node as next of prev_node
+        newNode.next = prevNode.next;
+        //make next of prev_node as new_node
+        prevNode.next = newNode;
+    }
+
+    private void append(Node head,int data){
+        Node endNode = new Node(data);
+        if (head == null) {head = endNode;}
+
+        Node curr = head;
+        /*
+            错误的方式，curr.next -> null
+
+            curr = curr.next 当curr = endNode的时候 是重新赋值给curr这个变量
+            curr的前节点已经指向NULL
+
+         */
+
+//        while (curr != null) {
+//            curr = curr.next;
+//        }
+//
+//        curr = endNode;
+
+        while(curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = endNode;
+        endNode.next = null;
+    }
+
+    private void printList(Node n){
         while(n != null) {
             System.out.println(n.data);
             n = n.next;
--- python/algorithm/datastruct/Linkedlist.py
+++ python/algorithm/datastruct/Linkedlist.py
@@ -9,10 +9,44 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-def printList(node):
-    while node:
-        print(node.data)
-        node = node.next
+    def printList(self,node):
+        while node:
+            print(node.data)
+            node = node.next
+
+    def push(self, data):
+        node0 = Node(0)
+        node0.next = self.head
+        return node0
+
+
+    def insertAfter(self,prevNode,data):
+        if prevNode is None:
+            print("The given previous node must inLinkedList.")
+            return
+
+        node = Node(data)
+        node.next = prevNode.next
+        prevNode.next = node
+
+    def append(self, data):
+        nodeEnd =  Node(data)
+        if self.head is None:
+            self.head = nodeEnd
+            return
+
+        curr = self.head
+        # while curr:
+        #     curr = curr.next
+
+        # curr = nodeEnd
+        # nodeEnd.next = None
+        while curr.next:
+            curr = curr.next
+
+        curr.next = nodeEnd
+
+
 
 if __name__ == "__main__":
     ll = LinkedList()
@@ -23,5 +57,23 @@ if __name__ == "__main__":
     ll.head.next = second
     second.next = third
 
-    printList(ll.head)
+    ll.printList(ll.head)
+    print("==========================")
+
+    # A node can be added in three ways
+    # At the front of the linked list
+    node0 = ll.push(0)
+    ll.printList(node0)
+
+    print("==========================")
+
+    # Add a node after a given node
+    ll.insertAfter(node0.next.next, 23)
+    ll.printList(node0)
+
+    print("==========================")
+
+    # Add a node at the end
+    ll.append(4)
+    ll.printList(node0)
 
